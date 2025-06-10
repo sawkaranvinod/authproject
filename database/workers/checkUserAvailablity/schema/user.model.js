cc5727e454fca6d53c536b1e53a45af1435723de
sawkaranvinod <saw_ug_24@ece.nits.ac.in>
2025-06-10 08:16:35 +0530

    done

--- database/workers/checkUserAvailablity/schema/user.model.js
+++ database/workers/checkUserAvailablity/schema/user.model.js
@@ -46,7 +46,6 @@ const userSchema = new mongoose.Schema(
         },
         TypeOfSecurityKey:{
             type: String,
-            lowercase:true,
             default: "none",
             enum: ['faceId', 'fingerprint', 'securityKey',"none"],
         },
---
+++ database/workers/registerServices/schema/loginDetail.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+// this is hashed by using personal detail hasing keys
+const loginDetailSchema = mongoose.Schema(
+    {
+        hashedIpAddress:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+        hashedCoordinate:{
+            type:{
+                longitude:{
+                    type:Number,
+                    trim:true,
+                },
+                latitude:{
+                    type:Number,
+                    trim:true,
+                }
+            },
+            required:true,
+        },
+        browser:{
+            type:String,
+            required:true,
+            trim:true,
+            lowercase:true,
+        },
+        deviceName:{
+            type:String,
+            required:true,
+            trim:true,
+            lowercase:true,
+        },
+        updatedUserProfile:{
+            type: {
+                updated:{
+                    type:Boolean,
+                    default:false,
+                },
+                updatedService:{
+                    type:String,
+                    lowercase:true
+                }
+            }
+        }
+    },
+    {
+        timestamps:true,
+    }
+);
+
+
+
+export const loginDetail = mongoose.model("loginDetail",loginDetailSchema);
